a0ed34b1ebabe5c7bea04b8ab9eac238600633c8
Flemmli97 <Flemmli97@users.noreply.github.com>
2024-07-02 13:27:34 +0200

    update dependencies and configs

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,8 +1,8 @@
 import nodeResolve from "@rollup/plugin-node-resolve"
 import commonjs from '@rollup/plugin-commonjs';
-import babel from 'rollup-plugin-babel';
-import { terser } from 'rollup-plugin-terser';
-import del from 'rollup-plugin-delete'
+import babel from '@rollup/plugin-babel';
+import terser from '@rollup/plugin-terser';
+import { del } from '@kineticcafe/rollup-plugin-delete'
 import typescript from '@rollup/plugin-typescript';
 
 export default {
